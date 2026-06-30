d42c1505c85b8d099c3d33758b072a33e71b0a8a
Arjun Pant <pantarjun234@gmail.com>
2026-07-01 03:39:38 +0530

    Trainer, evaluation and mlflow commited

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -5,6 +5,8 @@ import os,sys
 import numpy as np
 import dill
 import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 def read_yaml_file(file_path: str) -> dict:
     try:
@@ -22,7 +24,7 @@ def write_yaml_file(file_path: str, content: object, replace: bool=False)->None:
         with open(file_path, "w") as file:
             yaml.dump(content,file)
     except Exception as e:
-        raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e,sys) from e
     
 def save_numpy_array_data(file_path: str, array:np.array):
     try:
@@ -31,7 +33,7 @@ def save_numpy_array_data(file_path: str, array:np.array):
         with open(file_path, "wb") as file_obj:
             np.save(file_obj, array)
     except Exception as e:
-        raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e,sys) from e
     
 def save_object(file_path: str, obj: object) -> None:
     try:
@@ -41,4 +43,47 @@ def save_object(file_path: str, obj: object) -> None:
             pickle.dump(obj, file_obj)
         logging.info("Exited the save_object method of MainUtils class")
     except Exception as e:
-        raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e,sys) from e
+    
+def load_object(file_path: str, ) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file {file_path} is not exists")
+        with open(file_path,"rb") as file_obj:
+            print(file_obj)
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
+    
+def load_numpy_array_data(file_path: str) -> np.array:
+    try:
+        with open(file_path,"rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            
+            para = param[model_name]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred) 
+
+            report[model_name] = test_model_score
+
+        return report
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
